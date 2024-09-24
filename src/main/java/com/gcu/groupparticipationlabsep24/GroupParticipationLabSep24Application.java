5cbc18d7ae24033f0389f9929f1489cc7e3e3e12
Dima Bondar <dimabondar@Dimas-Macbook-Pro.local>
2024-09-24 15:47:20 -0700

    Initial commit

---
+++ src/main/java/com/gcu/groupparticipationlabsep24/GroupParticipationLabSep24Application.java
@@ -0,0 +1,13 @@
+package com.gcu.groupparticipationlabsep24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupParticipationLabSep24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupParticipationLabSep24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gcu/groupparticipationlabsep24/GroupParticipationLabSep24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.groupparticipationlabsep24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupParticipationLabSep24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
